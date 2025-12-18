6aec346732345aa16e8194f6e69ede8a3b8979ab
federicaminecra <51544094-federicaminecra@users.noreply.replit.com>
2025-12-18 06:31:38 +0000

    ttt

--- migrations/add-template-columns.ts
+++ migrations/add-template-columns.ts
@@ -1,6 +1,6 @@
 import pg from 'pg';
 
-const DATABASE_URL = "postgresql://postgres.hzyfajjndmttkpdsbayo:1NoxIsTheBest1!@aws-1-eu-central-1.pooler.supabase.com:6543/postgres";
+const DATABASE_URL = "";
 
 async function migrate() {
     const client = new pg.Client({ connectionString: DATABASE_URL });
